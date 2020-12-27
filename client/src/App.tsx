0c6f4424636d7ec714ab7ad625e807ac25b8c41d
yoni was <yonixw@gmail.com>
2020-12-27 20:45:34 +0200

    start ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+
+import { Row, Col, Checkbox , Typography,
+   Divider, Button, Space,Alert } from 'antd';
+
+import { Steps } from 'antd';
+
+const { Step } = Steps;
+const { Title, Paragraph, Text, Link } = Typography;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{paddingTop: "5%"}}> 
+      <Row>
+        <Col span={12} offset={6} className="card">
+          <Space direction="vertical">
+              <Title>Load or generate Encryption-Key</Title>
+              <Alert
+                message="Encryption-Key"
+                description="To encrypt and decrypt the data, or to make a new allowd timeslot, you will need an encryption key.
+                You can load your existing one through QR or generate a new one."
+                type="info"
+                showIcon
+              />
+              <Space>
+                <Button>Create new</Button>
+                <Button type="primary">Load Existing</Button>
+              </Space>
+
+              <Divider/>
+              
+              <Title>Create a new timeslot</Title>
+          </Space>
+        </Col>
+      </Row>
     </div>
   );
 }
