f633f349eed6e8cb04fdcaa0af78e1ddb42daa6d
yoni was <yonixw@gmail.com>
2020-12-29 23:10:40 +0200

    TODO finish "apiCreateData" + make scan + update same as put

--- server/src/dynamodb/apis.ts
+++ server/src/dynamodb/apis.ts
@@ -29,3 +29,28 @@ export async function apiCreateKey(
 
     return fastData({key: key, key_id: key_id});
 }
+
+export async function apiCreateData(
+    client: DynamoDBClient, body: CreateKeyCommandInput) : 
+        Promise<ExpressResult<CreateKeyCommandOutput>> {
+
+if (!body["user_id"])
+   return fastFail("Error: missing user_id in post");
+               
+const key = body["key"] || genKey(32);
+const key_id = getKeyID(key);
+const rowData: EncKeyRow = {
+    user_id: body.user_id,
+    key_id: key_id,
+    key: key
+};
+
+const putResult = await ddbCreateUpdateRow(
+    client, DDB_TABLES.ENC_KEYS, rowData
+);
+
+if (!putResult.ok)
+    return fastFail(putResult.error);
+
+return fastData({key: key, key_id: key_id});
+}
--- server/src/dynamodb/models.ts
+++ server/src/dynamodb/models.ts
@@ -33,3 +33,21 @@ export interface EncKeyRow {
     key_id: string;     // Range key
     key: string;
 }
+
+
+export interface CreateDataCommandInput {
+    data: string,
+    user_id: string,
+    key?: string,
+    key_id?: string,
+}
+
+export interface CreateDataCommandOutput {
+    data_id: string
+}
+
+export interface DataEncKeyRow {
+    user_id: string;    // Partition Key
+    key_id: string;     // Range key
+    enc_data: string;
+}
